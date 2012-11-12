746268c19c56787f41a1961a638ce68b594083f3
Bahaa Galal <bahaa.g.ali@gmail.com>
2012-11-12 15:38:00 +0200

    fixing a bug related to sending notification to the message sender himself

--- node/app.js
+++ node/app.js
@@ -5,6 +5,7 @@ redis = require('redis'),
 
 // create redis client
 rc = redis.createClient();
+redisClient = redis.createClient();
 
 // create http server to listen on for incoming socket connection requests
 server = http.createServer(function(req, res){
@@ -23,6 +24,17 @@ var socketio = io.listen(server);
 // console that server is listening
 console.log('server is running on port: 8000');
 
+// opened sockets variable
+var sockets = {};
+
+// connect and store user socket with his username
+socketio.on('connection', function(socket){
+    socket.on('presence online', function (userId) {
+        console.log('user ' + userId + ' is connected');
+        sockets[userId] = socket;
+    });
+});
+
 // subscribe with redis on the chat channel
 rc.on("connect", function() {
     console.log('rc is connected');
@@ -31,5 +43,32 @@ rc.on("connect", function() {
 
 // when a new message comes on the chat channel console.log it
 rc.on("message", function (channel, message) {
-    console.log("Message: " + message);
-})
+    console.log("message id " + message);
+    message_id = 'message:' + message;
+    redisClient.hgetall(message_id, function(err, reply){
+        message = reply;
+        thread_id = 'thread:' + message.thread_id + ':users';
+        user_id = 'user:' + message.sender_id;
+        redisClient.hgetall(user_id, function(err, reply){
+            user = reply;
+            messageObject = {
+                senderName: user.name,
+                senderAvatar: user.avatar,
+                body: message.body,
+                threadId: message.thread_id,
+                time: message.time
+            };
+            console.log("message is " + messageObject);
+            redisClient.smembers(thread_id, function(err, reply){
+                users = reply;
+                console.log("users are " + users);
+                for(i = 0, count = users.length; i < count; i++){
+                    if(users[i] != message.sender_id && users[i] in sockets){
+                        console.log(users[i]);
+                        sockets[users[i]].emit("message", messageObject);
+                    }
+                }
+            });
+        });    
+    });
+});
